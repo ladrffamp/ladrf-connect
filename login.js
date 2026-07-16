27c28362218dab428fbaa4ac49eb5086b9b93c93
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-16 09:30:49 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,34 +1,38 @@
 import { auth } from "./firebase.js";
 
 import {
-
-signInWithEmailAndPassword
-
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 window.login = async function(){
 
-const email =
-document.getElementById("email").value;
+  const email = document.getElementById("email").value.trim();
 
-const senha =
-document.getElementById("senha").value;
+  const senha = document.getElementById("senha").value;
 
-try{
 
-await signInWithEmailAndPassword(
-auth,
-email,
-senha
-);
+  try {
 
-window.location.href="dashboard.html";
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      senha
+    );
 
-}catch(e){
 
-document.getElementById("erro").innerHTML=
-"E-mail ou senha inválidos.";
+    alert("Login realizado com sucesso!");
 
-}
+    window.location.href = "dashboard.html";
+
+
+  } catch(error) {
+
+    console.log(error);
+
+    document.getElementById("erro").innerHTML =
+    "E-mail ou senha inválidos.";
+
+  }
 
 }
