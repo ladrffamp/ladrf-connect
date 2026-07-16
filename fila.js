0f6bd6824493f1546a85a71603fbbf58450fc5e8
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-16 00:33:57 -0300

    Update fila.js

--- fila.js
+++ fila.js
@@ -4,7 +4,9 @@ import {
   collection,
   onSnapshot,
   query,
-  orderBy
+  orderBy,
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
@@ -18,54 +20,112 @@ const fila = query(
 );
 
 
-onSnapshot(
-  fila,
-  (snapshot) => {
 
-    lista.innerHTML = "";
+onSnapshot(fila, (snapshot) => {
 
-    let total = 0;
+  lista.innerHTML = "";
+
+  let aguardando = 0;
 
 
-    snapshot.forEach((doc) => {
+  snapshot.forEach((item) => {
 
-      const paciente = doc.data();
+    const paciente = item.data();
 
-      total++;
+
+    if(paciente.status === "Aguardando"){
+
+      aguardando++;
 
 
       lista.innerHTML += `
 
       <div class="paciente">
 
-        <h2>${paciente.senha || "Sem senha"}</h2>
+        <h2>${paciente.senha}</h2>
 
         <p><b>${paciente.nome}</b></p>
 
         <p>${paciente.modalidade}</p>
 
+        <p>Queixa: ${paciente.queixa}</p>
+
         <p>Status: ${paciente.status}</p>
+
+
+        <select id="maca-${item.id}">
+
+          <option value="Maca 1">
+          Maca 1
+          </option>
+
+          <option value="Maca 2">
+          Maca 2
+          </option>
+
+          <option value="Maca 3">
+          Maca 3
+          </option>
+
+          <option value="Maca 4">
+          Maca 4
+          </option>
+
+        </select>
+
+
+        <button onclick="chamar('${item.id}')">
+
+        CHAMAR
+
+        </button>
+
 
       </div>
 
       `;
 
-    });
+    }
 
 
-    contador.innerHTML =
-    `🟡 ${total} paciente(s) na fila`;
+  });
 
 
-  },
+  contador.innerHTML =
+  `🟡 ${aguardando} paciente(s) aguardando`;
 
-  (erro) => {
 
-    lista.innerHTML =
-    "Erro ao carregar fila: " + erro.message;
+});
 
-    console.log(erro);
 
-  }
+
+
+window.chamar = async function(id){
+
+
+const maca = 
+document.getElementById(`maca-${id}`).value;
+
+
+
+await updateDoc(
+
+doc(db,"pacientes",id),
+
+{
+
+status:"Chamado",
+
+maca:maca
+
+}
 
 );
+
+
+alert(
+"Paciente chamado para " + maca
+);
+
+
+}
