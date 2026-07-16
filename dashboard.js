854ba7eeb086b7b9f3f7fcea225510500b881310
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-16 14:04:30 -0300

    Refactor dashboard to handle sign-out event
    Replace patient status display logic with sign-out functionality.

--- dashboard.js
+++ dashboard.js
@@ -1,56 +1,15 @@
-import { db } from "./firebase.js";
+import { auth } from "./firebase.js";
 
 import {
-collection,
-onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const cards = document.getElementById("cards");
+const sair = document.getElementById("sair");
 
-onSnapshot(collection(db,"pacientes"),(snapshot)=>{
+sair.addEventListener("click", async ()=>{
 
-let aguardando=0;
-let chamado=0;
-let finalizado=0;
+await signOut(auth);
 
-snapshot.forEach((doc)=>{
-
-const p=doc.data();
-
-if(p.status==="Aguardando") aguardando++;
-
-if(p.status==="Chamado") chamado++;
-
-if(p.status==="Finalizado") finalizado++;
-
-});
-
-cards.innerHTML=`
-
-<div class="card">
-
-<h2>🟡 ${aguardando}</h2>
-
-<p>Aguardando</p>
-
-</div>
-
-<div class="card">
-
-<h2>🔴 ${chamado}</h2>
-
-<p>Em Atendimento</p>
-
-</div>
-
-<div class="card">
-
-<h2>✅ ${finalizado}</h2>
-
-<p>Finalizados</p>
-
-</div>
-
-`;
+window.location.href="login.html";
 
 });
