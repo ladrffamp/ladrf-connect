c54e4fa8bdb7eeb40d85e5439c754549f0261b03
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-16 15:12:08 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,12 +1,48 @@
-import { auth } from "./firebase.js";
+import { db, auth } from "./firebase.js";
+
+import {
+collection,
+onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
 signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const sair = document.getElementById("sair");
+const pacientes=document.getElementById("pacientes");
+const espera=document.getElementById("espera");
+const atendimento=document.getElementById("atendimento");
+const finalizados=document.getElementById("finalizados");
 
-sair.addEventListener("click", async ()=>{
+onSnapshot(collection(db,"pacientes"),(snapshot)=>{
+
+let total=0;
+let aguardando=0;
+let atendendo=0;
+let finalizado=0;
+
+snapshot.forEach((doc)=>{
+
+total++;
+
+const status=doc.data().status;
+
+if(status==="Aguardando") aguardando++;
+
+if(status==="Em atendimento") atendendo++;
+
+if(status==="Finalizado") finalizado++;
+
+});
+
+pacientes.innerHTML=total;
+espera.innerHTML=aguardando;
+atendimento.innerHTML=atendendo;
+finalizados.innerHTML=finalizado;
+
+});
+
+document.getElementById("logout").addEventListener("click",async()=>{
 
 await signOut(auth);
 
