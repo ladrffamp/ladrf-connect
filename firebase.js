25cf8470f85b610be7167c8ebe4af4a8d8cb9e73
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-15 21:43:01 -0300

    Atualizar o firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
-index.html
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
+  authDomain: "ladrf-connect.firebaseapp.com",
+  projectId: "ladrf-connect",
+  storageBucket: "ladrf-connect.firebasestorage.app",
+  messagingSenderId: "863498841924",
+  appId: "1:863498841924:web:f3d97064e13cbdda893111"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
