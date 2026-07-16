c66d74ce2ca5b57b8a19508cae4745cb8fe21a17
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-16 10:07:53 -0300

    Update login.js

--- login.js
+++ login.js
@@ -7,9 +7,17 @@ import {
 
 window.login = async function(){
 
-  const email = document.getElementById("email").value.trim();
+  const email = document
+    .getElementById("email")
+    .value
+    .trim();
 
-  const senha = document.getElementById("senha").value;
+  const senha = document
+    .getElementById("senha")
+    .value;
+
+
+  const mensagem = document.getElementById("erro");
 
 
   try {
@@ -21,17 +29,62 @@ window.login = async function(){
     );
 
 
-    alert("Login realizado com sucesso!");
+    mensagem.style.color = "green";
+    mensagem.innerHTML = "Login realizado!";
 
-    window.location.href = "dashboard.html";
+
+    setTimeout(()=>{
+
+      window.location.href = "dashboard.html";
+
+    },1000);
 
 
   } catch(error) {
 
-    console.log(error);
 
-    document.getElementById("erro").innerHTML =
-    "E-mail ou senha inválidos.";
+    console.log("Código do erro:", error.code);
+
+    console.log("Mensagem:", error.message);
+
+
+    mensagem.style.color = "red";
+
+
+    if(error.code === "auth/user-not-found"){
+
+      mensagem.innerHTML =
+      "Usuário não encontrado.";
+
+    }
+
+    else if(error.code === "auth/wrong-password"){
+
+      mensagem.innerHTML =
+      "Senha incorreta.";
+
+    }
+
+    else if(error.code === "auth/invalid-credential"){
+
+      mensagem.innerHTML =
+      "E-mail ou senha inválidos.";
+
+    }
+
+    else if(error.code === "auth/operation-not-allowed"){
+
+      mensagem.innerHTML =
+      "Login por e-mail não está ativado no Firebase.";
+
+    }
+
+    else {
+
+      mensagem.innerHTML =
+      error.code;
+
+    }
 
   }
 
