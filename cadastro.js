bd1f6d3f220285b1f44aa8c2c837bc248122fbf3
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-17 08:25:35 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,57 +1,76 @@
 import { db } from "./firebase.js";
 
 import {
+
 collection,
 addDoc,
 Timestamp
+
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 
 
 
 const form = document.getElementById("formCadastro");
 
+const qr = document.getElementById("qrcode");
 
 
-form.addEventListener("submit", async (e)=>{
+
+
+
+form.addEventListener("submit", async(e)=>{
 
 
 e.preventDefault();
 
 
 
+
+
 const nome =
+
 document.getElementById("nome").value;
 
 
+
 const whatsapp =
+
 document.getElementById("whatsapp").value;
 
 
+
 const idade =
+
 document.getElementById("idade").value;
 
 
+
 const modalidade =
+
 document.getElementById("modalidade").value;
 
 
+
 const queixa =
+
 document.getElementById("queixa").value;
+
+
 
 
 
 try{
 
 
-await addDoc(
+
+// Criar paciente
+
+const pacienteCriado = await addDoc(
 
 collection(db,"pacientes"),
 
 {
-
-
-codigoAtendimento:
-"LADRF-" + Date.now(),
 
 
 nome,
@@ -76,6 +95,7 @@ status:"Aguardando",
 
 
 criadoEm:
+
 Timestamp.now()
 
 
@@ -86,7 +106,67 @@ Timestamp.now()
 
 
 
-alert("Paciente cadastrado com sucesso!");
+
+
+
+alert(
+"Paciente cadastrado com sucesso!"
+);
+
+
+
+
+
+
+
+
+// Gerar QR Code
+
+if(qr && typeof QRCode !== "undefined"){
+
+
+
+const link =
+
+`https://ladrffamp.github.io/ladrf-connect/acompanhamento.html?id=${pacienteCriado.id}`;
+
+
+
+
+
+qr.innerHTML="";
+
+
+
+
+new QRCode(
+
+qr,
+
+{
+
+
+text:link,
+
+
+width:200,
+
+
+height:200
+
+
+}
+
+);
+
+
+
+
+}
+
+
+
+
 
 
 
@@ -94,15 +174,26 @@ form.reset();
 
 
 
+
+
 }catch(error){
+
 
 
 console.error(error);
 
 
+
 alert(
-"Erro ao cadastrar paciente"
+
+"Erro ao cadastrar paciente: "
+
++
+
+error.message
+
 );
+
 
 
 }
