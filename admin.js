0118e9945b4e5af7027063c918fddfa8019767f2
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-17 09:54:56 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,53 +1,102 @@
 import { auth, db } from "./firebase.js";
 
-import {
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
+
+onAuthStateChanged
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+import {
+
 doc,
 getDoc
+
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
 
 
 onAuthStateChanged(auth, async (usuario)=>{
 
 
+
 if(!usuario){
+
 
 window.location.href="login.html";
 
+
 return;
+
 
 }
 
 
+
+
+
 const referencia = doc(
+
 db,
+
 "usuarios",
+
 usuario.uid
+
 );
+
+
+
 
 
 const dados = await getDoc(referencia);
 
 
+
+
+
+
 if(!dados.exists()){
 
-window.location.href="dashboard.html";
+
+window.location.href="index.html";
+
 
 return;
 
+
 }
+
+
+
+
+
 
 
 if(dados.data().perfil !== "admin"){
 
-alert("Acesso permitido apenas para administradores");
 
-window.location.href="dashboard.html";
+
+alert(
+"Acesso permitido apenas para administradores"
+);
+
+
+
+window.location.href="index.html";
+
+
+
+return;
+
 
 }
+
+
+
 
 
 });
