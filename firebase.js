25e2411344cc3a8a1fa2e14e329b56bbe18a7d98
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-18 02:22:37 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,100 +1,75 @@
-// firebase.js
+// =====================================
+// IMPORTAÇÕES FIREBASE
+// =====================================
 
-
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-
-import {
-
-getAuth
-
-} from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-getFirestore
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-} from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+import {
+  getMessaging
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
 
 // =====================================
 // CONFIGURAÇÃO FIREBASE LADRF CONNECT
 // =====================================
 
-
 const firebaseConfig = {
 
+  apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
 
-apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
+  authDomain: "ladrf-connect.firebaseapp.com",
 
+  projectId: "ladrf-connect",
 
-authDomain: "ladrf-connect.firebaseapp.com",
+  storageBucket: "ladrf-connect.firebasestorage.app",
 
+  messagingSenderId: "863498841924",
 
-projectId: "ladrf-connect",
+  appId: "1:863498841924:web:f3d97064e13cbdda893111",
 
-
-storageBucket: "ladrf-connect.firebasestorage.app",
-
-
-messagingSenderId: "863498841924",
-
-
-appId: "1:863498841924:web:f3d97064e13cbdda893111",
-
-
-measurementId: "G-0Q9WM2KH35"
-
+  measurementId: "G-0Q9WM2KH35"
 
 };
 
 
-
-
-
 // =====================================
-// INICIALIZAÇÃO
+// INICIALIZAÇÃO DO FIREBASE
 // =====================================
-
 
 const app = initializeApp(firebaseConfig);
-
-
-
 
 
 // =====================================
 // SERVIÇOS
 // =====================================
 
-
 const auth = getAuth(app);
-
 
 const db = getFirestore(app);
 
-
-
-
+const messaging = getMessaging(app);
 
 
 // =====================================
-// EXPORTAR
+// EXPORTAÇÕES
 // =====================================
-
 
 export {
 
+  app,
 
-app,
+  auth,
 
-auth,
+  db,
 
-db
+  messaging
 
 };
