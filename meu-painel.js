db7a260f76402765fa2cd6ced9ebcd2fdf499cb6
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-18 17:55:09 -0300

    Add main functionality for user authentication and events

---
+++ meu-painel.js
@@ -0,0 +1,119 @@
+import { auth, db } from "./firebase.js";
+
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const boasVindas =
+document.getElementById("boasVindas");
+
+const listaEscalas =
+document.getElementById("listaEscalas");
+
+onAuthStateChanged(
+
+auth,
+
+async(usuario)=>{
+
+if(!usuario){
+
+window.location.href="login.html";
+
+return;
+
+}
+
+boasVindas.innerHTML =
+`👋 Bem-vindo`;
+
+carregarEscalas();
+
+}
+
+);
+
+async function carregarEscalas(){
+
+listaEscalas.innerHTML="";
+
+const eventos =
+await getDocs(
+
+collection(
+db,
+"agenda"
+)
+
+);
+
+if(eventos.empty){
+
+listaEscalas.innerHTML=`
+
+<div class="card">
+
+Nenhuma escala disponível.
+
+</div>
+
+`;
+
+return;
+
+}
+
+eventos.forEach((evento)=>{
+
+const dados =
+evento.data();
+
+listaEscalas.innerHTML += `
+
+<div class="escala">
+
+<h3>
+
+${dados.titulo || "Evento"}
+
+</h3>
+
+<p>
+
+📅 ${dados.data || "-"}
+
+</p>
+
+<p>
+
+📍 ${dados.local || "-"}
+
+</p>
+
+<p>
+
+⏰ ${dados.inicio || "-"}
+às
+${dados.fim || "-"}
+
+</p>
+
+<button
+class="confirmar">
+
+✅ Confirmar Presença
+
+</button>
+
+</div>
+
+`;
+
+});
+
+}
