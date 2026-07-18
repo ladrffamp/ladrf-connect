9683eb4e54cc40db3bf7d8cc25fc4862559da3ca
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-18 02:23:17 -0300

    Refactor Firebase messaging service worker code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,46 +1,41 @@
-importScripts(
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"
-);
-
-importScripts(
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js"
-);
-
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-
-apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
-
-authDomain: "ladrf-connect.firebaseapp.com",
-
-projectId: "ladrf-connect",
-
-storageBucket: "ladrf-connect.firebasestorage.app",
-
-messagingSenderId: "863498841924",
-
-appId: "1:863498841924:web:f3d97064e13cbdda893111"
-
+  apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
+  authDomain: "ladrf-connect.firebaseapp.com",
+  projectId: "ladrf-connect",
+  storageBucket: "ladrf-connect.firebasestorage.app",
+  messagingSenderId: "863498841924",
+  appId: "1:863498841924:web:f3d97064e13cbdda893111"
 });
-
 
 const messaging = firebase.messaging();
 
+messaging.onBackgroundMessage((payload) => {
+  console.log("Mensagem recebida:", payload);
 
-messaging.onBackgroundMessage((payload)=>{
+  const notificationTitle =
+    payload.notification?.title || "LADRF Connect";
 
+  const notificationOptions = {
+    body: payload.notification?.body || "Chegou sua vez!",
+    icon: "/icons/icon-192.png",
+    badge: "/icons/icon-192.png",
+    data: payload.data || {},
+    requireInteraction: true
+  };
 
-self.registration.showNotification(
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
 
-payload.notification.title,
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
 
-{
-
-body: payload.notification.body
-
-}
-
-);
-
-
+  event.waitUntil(
+    clients.openWindow("/")
+  );
 });
