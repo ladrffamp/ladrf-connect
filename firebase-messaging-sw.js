0f15c877f3fe272914f30aa37f1106ccfb4c9fb5
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-17 23:51:43 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,13 @@
-// firebase-messaging-sw.js
-
-
 importScripts(
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"
 );
-
 
 importScripts(
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js"
 );
 
 
-
-// Configuração Firebase LADRF Connect
-
-const firebaseConfig = {
-
+firebase.initializeApp({
 
 apiKey: "AIzaSyC7hogEzFpAOzPiKsc7FkQnFrCOveZOfos",
 
@@ -27,77 +19,31 @@ storageBucket: "ladrf-connect.firebasestorage.app",
 
 messagingSenderId: "863498841924",
 
-appId: "1:863498841924:web:f3d97064e13cbdda893111",
+appId: "1:863498841924:web:f3d97064e13cbdda893111"
 
-measurementId: "G-0Q9WM2KH35"
-
-
-};
-
-
-
-
-// Inicializar Firebase
-
-firebase.initializeApp(firebaseConfig);
-
+});
 
 
 const messaging = firebase.messaging();
 
 
 
-
-// Receber notificações com navegador fechado
-
-messaging.onBackgroundMessage(
-
-(payload)=>{
-
-
-console.log(
-
-"Notificação recebida em segundo plano:",
-
-payload
-
-);
-
-
-
-const titulo =
-
-payload.notification.title || "LADRF Connect";
-
-
-
-const opcoes = {
-
-
-body:
-
-payload.notification.body || "Nova atualização",
-
-
-icon:
-
-"/icon.png"
-
-
-};
-
+messaging.onBackgroundMessage((payload)=>{
 
 
 self.registration.showNotification(
 
-titulo,
+payload.notification.title,
 
-opcoes
+{
 
-);
+body: payload.notification.body,
 
-
+icon: "/favicon.ico"
 
 }
 
 );
+
+
+});
