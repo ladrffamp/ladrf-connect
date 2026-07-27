a9a5026375f78808be1e04567ff0af7263f5752d
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-27 14:16:05 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -13,10 +13,18 @@ doc,
 
 updateDoc,
 
-deleteDoc
+deleteDoc,
+
+setDoc
 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
+import {
+
+createUserWithEmailAndPassword
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
 // ELEMENTOS
