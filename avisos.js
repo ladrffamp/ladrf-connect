2cc3d6aeb90f9b8c06feee30cdba85c31602bfa9
ladrffamp <ladrf.fampfaculdade@gmail.com>
2026-07-27 10:02:22 -0300

    Add avisoEditando variable for editing state

--- avisos.js
+++ avisos.js
@@ -12,7 +12,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const avisosRef = collection(db, "avisos");
-
+let avisoEditando = null;
 
 
 // ======================================
